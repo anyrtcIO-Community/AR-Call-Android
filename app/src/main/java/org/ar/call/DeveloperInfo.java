470e3024bfcd77c2c7a744b97353af1eb5a9c4d8
liuxiaozhong <liuxiaozhong@dync.cc>
2020-02-22 19:10:26 +0800

    update

--- app/src/main/java/org/ar/call/DeveloperInfo.java
+++ app/src/main/java/org/ar/call/DeveloperInfo.java
@@ -5,9 +5,9 @@ package org.ar.call;
 public class DeveloperInfo {
 
 
-    public final static String APPID = "";
+    public final static String APPID = "anyrtcSt4zzRtGTR3q";
 
-    public final static String APPTOKEN = "";
+    public final static String APPTOKEN = "bb4366d7d8f3c5e839e5847eab83ff2f";
 
     public final static String USERID = "userid";
 
